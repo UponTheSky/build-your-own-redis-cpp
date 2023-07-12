7f9140e71e014ccd15737715821ce7be4854d4a8
UponTheSky <sailing.to.orbis@gmail.com>
2023-07-12 09:11:04 +0900

    sec 6: WIP

--- src/my_redis/server.cpp
+++ src/my_redis/server.cpp
@@ -41,6 +41,82 @@ int32_t Server::one_request(int connfd) {
   return Utils::write_all(connfd, wbuf, 4 + len);
 }
 
+void Server::fd_set_nb(int fd) {
+  errno = 0;
+  int flags = fcntl(fd, F_GETFL, 0);
+  if (errno) {
+    Utils::die("fcntl error");
+    return;
+  }
+
+  flags |= O_NONBLOCK;
+
+  errno = 0;
+  fcntl(fd, F_SETFL, flags);
+  if (errno) {
+    Utils::die("fcntl error");
+  }
+}
+
+void conn_put(std::map<int, Conn*>& fd2conn, struct Conn* conn) {
+  fd2conn[conn->fd] = conn;
+}
+
+int32_t Server::accept_new_conn(std::map<int, Conn*>& fd2conn, int fd) {
+  // accept
+    struct sockaddr_in client_addr = {};
+    socklen_t socklen = sizeof(client_addr);
+    int connfd = accept(fd, (struct sockaddr*)&client_addr, &socklen);
+    if (connfd < 0) {
+      Utils::msg("accept() error");
+      return -1;
+    }
+
+    // set the new connfd as nonblocking
+    fd_set_nb(connfd);
+
+    // create a struct Conn
+    struct Conn* conn = new Conn;
+
+    conn->fd = connfd;
+    conn->state = State::STATE_REQ;
+    conn_put(fd2conn, conn);
+
+    return 0;
+}
+
+void Server::connection_io(Conn* conn) {
+  switch (conn->state) {
+    case State::STATE_REQ:
+      state_req(conn);
+      break;
+    case State::STATE_RES:
+      state_res(conn);
+      break;
+    default:
+      assert(0);
+  }
+}
+
+void Server::state_req(Conn* conn) {
+  while (try_fill_buffer(conn)) {}
+}
+
+bool Server::try_fill_buffer(Conn* conn) {
+  assert(conn->rbuf_size < sizeof(conn->rbuf));
+  ssize_t rv = 0;
+
+  do {
+    size_t cap = sizeof(conn->rbuf) - conn->rbuf_size;
+    rv = read(conn->fd, &conn->rbuf[conn->rbuf_size], cap);
+  } while (rv < 0 && errno == EINTR);
+
+  if (rv < 0 && errno == EAGAIN) {
+    return false;
+  }
+
+}
+
 void Server::run() {
   // step 1: generate a socket(ipv4, tcp)
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -69,23 +145,63 @@ void Server::run() {
     Utils::die("listen ()");
   }
 
+  // step 5: get ready for the event loop
+  fd_set_nb(sockfd);
+  std::map<int, Conn*> fd2conn;
+  std::vector<struct pollfd> poll_args;
+
   // infinite loop that runs until the user connection is remained
   while (true) {
-    // step 5: accept the request
-    struct sockaddr_in client_addr = {};
-    socklen_t socklen = sizeof(client_addr);
-    int connfd = accept(sockfd, (struct sockaddr*)&client_addr, &socklen);
-    if (connfd < 0) {
-      continue;
+    // step 6: prepare the args of poll()
+    poll_args.clear();
+    struct pollfd pfd = { sockfd, POLLIN, 0 };
+    poll_args.push_back(pfd);
+
+    for (const auto& [connfd, conn] : fd2conn) {
+      if (!conn) {
+        continue;
+      }
+
+      struct pollfd pfd = {};
+      pfd.fd = conn->fd,
+      pfd.events = (conn->state == State::STATE_REQ ? POLLIN : POLLOUT) | POLL_ERR;
+
+      poll_args.push_back(pfd);
     }
 
-    // step 6: parse the request one by one
-    while (true) {
-      int32_t err = one_request(connfd);
-      if (err) {
-        break;
+    // step 7: poll for the active fds
+    int rv = poll(poll_args.data(), (nfds_t)poll_args.size(), 1000); // 1000 => timeout
+    if (rv < 0) {
+      Utils::die("poll");
+    }
+
+    // step 8: process those active conns
+    for (size_t i = 1; i < poll_args.size(); i++) {
+      if (poll_args[i].revents)  { // if event has returned
+        Conn* conn = fd2conn[poll_args[i].fd];
+        connection_io(conn);
+        if (conn->state == State::STATE_END) {
+          // destroy the connection
+          fd2conn[conn->fd] = nullptr;
+          close(conn->fd);
+          delete conn;
+        }
       }
     }
-    close(connfd);
+
+    // step 8: if the listening sockfd is active, try to accept a new connection
+    if (poll_args[0].revents) {
+      accept_new_conn(fd2conn, sockfd);
+    }
+
+  //   // step 6: parse the request one by one
+  //   while (true) {
+  //     int32_t err = one_request(connfd);
+  //     if (err) {
+  //       break;
+  //     }
+  //   }
+  //   close(connfd);
+  // }
   }
 }
--- src/my_redis/server.h
+++ src/my_redis/server.h
@@ -7,9 +7,13 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <string.h>
+#include <fcntl.h>
+#include <poll.h>
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
 
 #include "utils.h"
 
@@ -22,6 +26,30 @@ class Server {
 
   private:
     int32_t one_request(int connfd);
+    void fd_set_nb(int fd);
+    void conn_put(std::map<int, Conn*>& fd2conn, struct Conn* conn);
+    int32_t accept_new_conn(std::map<int, Conn*>& fd2conn, int fd);
+    void connection_io(Conn* conn);
+    void state_req(Conn* conn);
+    void state_res(Conn* conn);
+    bool try_fill_buffer(Conn* conn);
+};
+
+// the state of an ongoing connection
+enum State: uint32_t {
+  STATE_REQ = 0, // reading requests
+  STATE_RES = 1, // sending responses
+  STATE_END = 2
+};
+
+struct Conn {
+  int fd = -1;
+  State state = State::STATE_REQ;
+  size_t rbuf_size = 0;
+  uint8_t rbuf[4 + K_MAX_MSG];
+  size_t wbuf_size = 0;
+  size_t wbuf_sent = 0;
+  uint8_t wbuf[4 + K_MAX_MSG];
 };
 
 #endif
